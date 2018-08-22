118cb9196d99450420d16d95f8f3c84fdfaaf741
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-08-22 15:44:05 -0400

    feat(index.js): Refactor logic to have just create & resolve endpoints

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const debug = require('debug')('ara:network:node:identity-manager')
+const { unpack, keyRing, derive } = require('ara-network/keys')
 const { createChannel } = require('ara-network/discovery/channel')
 const { createServer } = require('ara-network/discovery')
 const { info, warn, error } = require('ara-console')
-const secrets = require('ara-network/secrets')
+const ss = require('ara-secret-storage')
 const context = require('ara-context')()
+const crypto = require('ara-crypto')
 const aid = require('ara-identity')
+const { readFile } = require('fs')
+const { writeIdentity } = require('ara-identity/util')
+const { DID } = require('did-uri')
 const { resolve } = require('path')
 const express = require('express')
+const inquirer = require('inquirer')
 const extend = require('extend')
 const http = require('http')
-
+const rc = require('./rc')()
+const pify = require('pify')
+const pkg = require('./package')
 
 // in milliseconds
-const kRequestTimeout = 5000
+const kRequestTimeout = 10000
 
 const conf = {
   // in milliseconds
   'dns-announce-interval': 1000 * 60 * 2,
   // in milliseconds
   'dht-announce-interval': 1000 * 60 * 2,
-  archiverKey: null,
   keystore: null,
   port: 8000,
   key: null
@@ -36,66 +43,73 @@ async function getInstance() {
 async function configure(opts, program) {
   if (program) {
     const { argv } = program
-      .option('port', {
-        type: 'number',
-        alias: 'p',
-        describe: 'Port for network server to listen on.',
-        default: conf.port
+      .option('identity', {
+        alias: 'i',
+        describe: 'Ara Identity for the network node'
       })
-      .option('key', {
-        type: 'string',
+      .option('secret', {
+        alias: 's',
+        describe: 'Shared secret key'
+      })
+      .option('name', {
+        alias: 'n',
+        describe: 'Human readable network keys name.'
+      })
+      .option('keyring', {
         alias: 'k',
-        describe: 'Network key string',
+        describe: 'Path to ARA network keys'
       })
-      .option('archiverKey', {
-        type: 'string',
-        alias: 'K',
-        describe: 'Archiver Network key string',
+      .option('port', {
+        alias: 'p',
+        describe: 'Port for network node to listen on.'
       })
-
-
-    extend(true, conf, argv)
   }
 
   return extend(true, conf, opts)
 }
 
 async function start(argv) {
-
-  const keys = {
-    discoveryKey: null,
-    remote: null,
-    client: null,
-    network: null,
+  if (channel) {
+    return false
   }
 
-  if (null === conf.key || 'string' !== typeof conf.key) {
-    throw new TypeError('Expecting manager network key to be a string.')
-  }
+  channel = createChannel({ })
 
-  if (null === conf.archiverKey || 'string' !== typeof conf.archiverKey) {
-    throw new TypeError('Expecting archiver network key to be a string.')
+  let { password } = await inquirer.prompt([
+    {
+      type: 'password',
+      name: 'password',
+      message:
+        'Please enter the passphrase associated with the node identity.\n' +
+        'Passphrase:'
+    }
+  ])
+  if (0 !== argv.identity.indexOf('did:ara:')) {
+    argv.identity = `did:ara:${conf.identity}`
   }
+  const did = new DID(argv.identity)
+  const publicKey = Buffer.from(did.identifier, 'hex')
 
-  try {
-    const doc = await secrets.load(conf)
-    const { keystore } = doc.public || doc.secret
-    Object.assign(keys, secrets.decrypt({ keystore }, { key: conf.key }))
-  } catch (err) {
-    debug(err)
-    throw new Error(`Unable to read keystore for '${conf.key}'.`)
-  }
+  password = crypto.blake2b(Buffer.from(password))
 
-  Object.assign(conf, { discoveryKey: keys.discoveryKey })
-  Object.assign(conf, {
-    network: keys.network,
-    client: keys.client,
-    remote: keys.remote,
-  })
+  const hash = crypto.blake2b(publicKey).toString('hex')
+  const path = resolve(rc.network.identity.root, hash, 'keystore/ara')
+  const secret = Buffer.from(argv.secret)
+  const keystore = JSON.parse(await pify(readFile)(path, 'utf8'))
+  const secretKey = ss.decrypt(keystore, { key: password.slice(0, 16) })
+
+  const keyring = keyRing(argv.keyring, { secret: secretKey })
+  const buffer = await keyring.get(argv.name)
+  const unpacked = unpack({ buffer })
+
+  const { discoveryKey } = unpacked
+
+  info('%s: discovery key:', pkg.name, discoveryKey.toString('hex'))
 
   app = express()
 
-  app.post('/api/v1/create/', onidentifier)
+  app.post('/api/v1/create/', oncreate)
+  app.post('/api/v1/resolve/', onresolve)
 
   server = http.createServer(app)
   channel = createChannel({
@@ -108,7 +122,7 @@ async function start(argv) {
   })
   return true
 
-  async function onidentifier(req, res) {
+  async function oncreate(req, res) {
     let id = setTimeout(() => { res.status(408).send('Request Timed Out') }, kRequestTimeout)
     try{
       if (undefined === req.query.passphrase) {
@@ -117,15 +131,37 @@ async function start(argv) {
       else {
         const password = req.query.passphrase
         const identifier = await aid.create({ context, password })
-        const doc = await secrets.load({key: conf.archiverKey})
-        const { keystore } = (doc.public || doc.secret)
-        await aid.archive(identifier,{key: conf.archiverKey, keystore})
+        await writeIdentity(identifier)
         const response = {
           mnemonic: identifier.mnemonic,
           ddo: identifier.ddo
         }
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify(response))
+        res.on('finish', () => { clearTimeout(id) })
+      }
+    } catch (err){
+      debug(err)
+    }
+  }
+
+  async function onresolve(req, res) {
+    let id = setTimeout(() => { res.status(408).send('Request Timed Out') }, kRequestTimeout)
+    try{
+      if (undefined === req.query.did) {
+        res.status(401).send("Missing DID").end()
+      }
+      else {
+        if (0 !== req.query.did.indexOf('did:ara:')) {
+          req.query.did = `did:ara:${req.query.did}`
+        }
+        const did = new DID(req.query.did)
+        const publicKey = Buffer.from(did.identifier, 'hex')
+        const hash = crypto.blake2b(publicKey).toString('hex')
+        const path = resolve(rc.network.identity.root, hash, 'ddo.json')
+        const ddo = JSON.parse(await pify(readFile)(path, 'utf8'))
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(ddo))
         res.on('finish', () => { clearTimeout(id) })
       }
     } catch (err){
@@ -141,8 +177,8 @@ async function start(argv) {
 
   function announce() {
     const { port } = server.address()
-    info('identity-manager: Announcing %s on port %s', conf.discoveryKey.toString('hex'), port)
-    channel.join(conf.discoveryKey, port)
+    info('identity-manager: Announcing %s on port %s', discoveryKey.toString('hex'), port)
+    channel.join(discoveryKey, port)
   }
 }
 
