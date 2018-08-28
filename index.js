e9bdf46c48e1c8cf5473a442d741526f9bea451f
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2018-08-28 11:09:49 -0400

    feat(): Linting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-shadow */
 const debug = require('debug')('ara:network:node:identity-manager')
 const { createChannel } = require('ara-network/discovery/channel')
 const { unpack, keyRing } = require('ara-network/keys')
-const { createServer } = require('ara-network/discovery')
 const { writeIdentity } = require('ara-identity/util')
-const { info, warn, error } = require('ara-console')
+const { info, warn } = require('ara-console')
 const ss = require('ara-secret-storage')
 const context = require('ara-context')()
 const inquirer = require('inquirer')
@@ -14,7 +14,6 @@ const coalesce = require('defined')
 const { readFile } = require('fs')
 const { DID } = require('did-uri')
 const express = require('express')
-const extend = require('extend')
 const pkg = require('./package')
 const https = require('https')
 const http = require('http')
@@ -22,11 +21,10 @@ const rc = require('./rc')()
 const pify = require('pify')
 const fs = require('fs')
 
-
 // in milliseconds
 const kRequestTimeout = 5000
 
-let conf = {
+const conf = {
   port: 8000,
   identity: null,
   secret: null,
@@ -39,7 +37,6 @@ let conf = {
 let server = null
 let channel = null
 let app = null
-
 
 async function getInstance() {
   return server
@@ -78,7 +75,7 @@ async function configure(opts, program) {
         describe: 'Path to ssl certificate file for the server'
       })
       // eslint-disable-next-line prefer-destructuring
-      argv = program.argv
+    argv = program.argv
   }
   conf.port = select('port', argv, opts, conf)
   conf.name = select('name', argv, opts, conf)
@@ -99,7 +96,7 @@ async function start(argv) {
   if (channel) {
     return false
   }
-  console.log(conf)
+
   channel = createChannel({ })
 
   let { password } = await inquirer.prompt([
@@ -121,7 +118,7 @@ async function start(argv) {
 
   const hash = crypto.blake2b(publicKey).toString('hex')
   const path = resolve(rc.network.identity.root, hash, 'keystore/ara')
-  const secret = Buffer.from(conf.secret)
+  // const secret = Buffer.from(conf.secret)
   const keystore = JSON.parse(await pify(readFile)(path, 'utf8'))
   const secretKey = ss.decrypt(keystore, { key: password.slice(0, 16) })
 
@@ -144,8 +141,7 @@ async function start(argv) {
       cert: fs.readFileSync(resolve(conf.sslCert))
     }
     server = https.createServer(certOpts, app)
-  }
-  else {
+  } else {
     server = http.createServer(app)
   }
   channel = createChannel({
@@ -159,49 +155,45 @@ async function start(argv) {
   return true
 
   async function oncreate(req, res) {
-    let timer = setTimeout(() => { res.status(408).send('Request Timed Out') }, kRequestTimeout)
-    try{
+    const timer = setTimeout(() => { res.status(408).send('Request Timed Out') }, kRequestTimeout)
+    try {
       if (undefined === req.query.passphrase) {
-        res.status(401).send("Missing Passphrase").end()
+        res.status(401).send('Missing Passphrase').end()
         clearTimeout(timer)
-      }
-      else {
+      } else {
         info('%s: Received create request', pkg.name)
-        const password = req.query.passphrase
-        const identifier = await aid.create({ context, password })
+        const identifier = await aid.create({ context, password: req.query.passphrase })
         const id = `did:ara:${identifier.publicKey.toString('hex')}`
         await writeIdentity(identifier)
         const response = {
           did: id,
-          mnemonic: identifier.mnemonic,
-          ddo: identifier.ddo
+          mnemonic: identifier.mnemonic
         }
         info('%s: New Identity created successfully: %s', pkg.name, id)
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify(response))
         res.on('finish', () => { clearTimeout(timer) })
       }
-    } catch (err){
+    } catch (err) {
       debug(err)
-      res.status(401).send("Create request failed").end()
+      res.status(401).send('Create request failed').end()
       clearTimeout(timer)
     }
   }
 
   async function onresolve(req, res) {
-    let timer = setTimeout(() => { res.status(408).send('Request Timed Out') }, kRequestTimeout)
-    try{
+    const timer = setTimeout(() => { res.status(408).send('Request Timed Out') }, kRequestTimeout)
+    try {
       if (undefined === req.query.did) {
-        res.status(401).send("Missing DID").end()
+        res.status(401).send('Missing DID').end()
         clearTimeout(timer)
-      }
-      else {
+      } else {
         if (0 !== req.query.did.indexOf('did:ara:')) {
           req.query.did = `did:ara:${req.query.did}`
         }
         info('%s: Resolve request received for: %s', pkg.name, req.query.did)
-        const did = new DID(req.query.did)
-        const publicKey = Buffer.from(did.identifier, 'hex')
+        const id = new DID(req.query.did)
+        const publicKey = Buffer.from(id.identifier, 'hex')
         const hash = crypto.blake2b(publicKey).toString('hex')
         const path = resolve(rc.network.identity.root, hash, 'ddo.json')
         const ddo = JSON.parse(await pify(readFile)(path, 'utf8'))
@@ -210,9 +202,9 @@ async function start(argv) {
         res.end(JSON.stringify(ddo))
         res.on('finish', () => { clearTimeout(timer) })
       }
-    } catch (err){
+    } catch (err) {
       debug(err)
-      res.status(401).send("Resolve request failed. Invalid DID").end()
+      res.status(401).send('Resolve request failed. Invalid DID').end()
       clearTimeout(timer)
     }
   }
