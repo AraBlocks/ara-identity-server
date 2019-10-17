8083a771c793d0eb518cb77854aa5becffc4b990
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2019-10-17 13:38:06 -0400

    feat(): Add config folder

---
+++ config/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+
+
+/*
+Status Variables
+ */
+
+const status = {
+ internalServerError: 500,
+ requestTimeout: 408,
+ notImplemented: 501,
+ badRequest: 400,
+ notFound: 404,
+ ok: 200,
+ authenticationError: 401
+}
+
+ /*
+ Response Messages
+  */
+
+const msg = {
+  requestTimeout: `Request timed out.\n`,
+  authenticationFailed: 'Missing or invalid authentication credentials. \n',
+  status: 'Ara Identity Manager up and running. \n'
+}
+
+/*
+Constant Values
+ */
+const server_values = {
+  MAX_TOKEN_PER_ACCOUNT: 500,
+  DEFAULT_TOKEN_COUNT: 100,
+  TRANSFER_TIMEOUT: 90000,
+  REQUEST_TIMEOUT: 5000
+}
+
+ module.exports = {
+   server_values,
+   status,
+   msg
+ }
