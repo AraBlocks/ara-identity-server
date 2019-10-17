5c1c405f68f384590778ab2015e94b2257a58a28
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2019-10-17 13:41:42 -0400

    feat(index.js): Refactor to make modularize routes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { readFile } = require('fs')
 const { info, warn } = require('ara-console')
 const { parse: parseDID } = require('did-uri')
 const { createChannel } = require('ara-network/discovery/channel')
-const { writeIdentity } = require('ara-identity/util')
 const { resolve } = require('path')
 const bodyParser = require('body-parser')
 const { token } = require('ara-contracts')
@@ -23,11 +22,20 @@ const pify = require('pify')
 const util = require('ara-util')
 const aid = require('ara-identity')
 const pkg = require('./package')
-const rc = require('./rc')()
+const rc = require('./config/rc')()
+
+const {
+  ontransfer,
+  onbalance,
+  onresolve,
+  oncreate,
+  onredeem
+} = require('./routes')
 
 // in milliseconds
 const REQUEST_TIMEOUT = 5000
 
+//const DEFAULT_GAS_PRICE =
 const DEFAULT_TOKEN_COUNT = 100
 const MAX_TOKEN_PER_ACCOUNT = 500
 
@@ -135,15 +143,7 @@ async function start() {
   let discoveryKey = null
 
   channel = createChannel({ })
-  redisClient = redis.createClient()
 
-  redisClient.on('connect', () => {
-    info('Redis client connected')
-  })
-
-  redisClient.on('error', (err) => {
-    info(`Something went wrong ${err}`)
-  })
 
   await context.ready()
 
@@ -239,271 +239,6 @@ async function start() {
     res
       .status(status.ok)
       .end()
-  }
-
-  async function oncreate(req, res) {
-    const timer = setTimeout(() => {
-      res
-        .status(status.requestTimeout)
-        .send(msg.requestTimeout)
-    }, REQUEST_TIMEOUT)
-
-    try {
-      if (undefined === req.body.passphrase) {
-        res
-          .status(status.badRequest)
-          .send('Missing Passphrase parameter.\n')
-          .end()
-        clearTimeout(timer)
-      } else if ('' === req.body.passphrase) {
-        res
-          .status(status.badRequest)
-          .send('Missing Passphrase parameter value.\n')
-          .end()
-        clearTimeout(timer)
-      } else {
-        res.status(status.ok)
-        info('%s: Received create request', pkg.name)
-        const identifier = await aid.create({
-          context,
-          password: req.body.passphrase
-        })
-        await writeIdentity(identifier)
-
-        const did = `did:ara:${identifier.publicKey.toString('hex')}`
-        const walletAddress = await util.getAddressFromDID(did)
-
-        // Write Entry into Redis Store
-        redisClient.set(did, 0, 'EX', 60, (err, res) => {
-          if (err) {
-            debug(err)
-          } else {
-            debug(res)
-          }
-        })
-
-        const response = {
-          did,
-          mnemonic: identifier.mnemonic,
-          ddo: identifier.ddo,
-          walletAddress
-        }
-        info('%s: New Identity created successfully: %s', pkg.name, did)
-
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(response))
-        res.on('finish', () => { clearTimeout(timer) })
-      }
-    } catch (err) {
-      res
-        .status(status.internalServerError)
-        .send('Create request failed. \n')
-        .end()
-      debug(err)
-      clearTimeout(timer)
-    }
-  }
-
-  async function onresolve(req, res) {
-    const now = Date.now()
-
-    const timer = setTimeout(() => {
-      res
-        .status(status.requestTimeout)
-        .send(msg.requestTimeout)
-    }, REQUEST_TIMEOUT)
-
-    try {
-      if (0 !== req.params.did.indexOf('did:ara:')) {
-        req.params.did = `did:ara:${req.query.did}`
-      }
-      res.status(status.ok)
-      info('%s: Resolve request received for: %s', pkg.name, req.params.did)
-
-      try {
-        const did = new DID(req.params.did)
-        const publicKey = Buffer.from(did.identifier, 'hex')
-        const hash = crypto.blake2b(publicKey).toString('hex')
-        const path = resolve(conf.path, hash, 'ddo.json')
-        const ddo = JSON.parse(await pify(readFile)(path, 'utf8'))
-        const duration = Date.now() - now
-        const response = createResponse({ did, ddo, duration })
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(response))
-        res.on('finish', () => { clearTimeout(timer) })
-        debug('%s: Resolve request completed successfully.', pkg.name)
-      } catch (e) {
-        res
-          .status(status.notFound)
-          .send(`Could not resolve DID: ${req.params.did}\n`)
-          .end()
-        debug(status.notFound, e)
-        clearTimeout(timer)
-      }
-    } catch (err) {
-      debug(err)
-      res
-        .status(status.internalServerError)
-        .send(`Could not resolve DID: ${req.params.did}\n`)
-        .end()
-      debug(status.internalServerError, err)
-      clearTimeout(timer)
-    }
-  }
-
-  async function onbalance(req, res) {
-    const timer = setTimeout(() => {
-      res
-        .status(status.requestTimeout)
-        .send(msg.requestTimeout)
-    }, REQUEST_TIMEOUT)
-
-    try {
-      if (0 !== req.params.did.indexOf('did:ara:')) {
-        req.params.did = `did:ara:${req.query.did}`
-      }
-      res.status(status.ok)
-      info('%s: Balance request for %s', pkg.name, req.params.did)
-      const { did } = req.params
-      let balance = null
-      redisClient.get(did, async (err, bal) => {
-        if (null == bal) {
-          debug('Balance expired, retrieving from blockchain')
-          balance = await token.balanceOf(did)
-          redisClient.set(did, balance, 'EX', 60, (err, res) => {
-            if (err) {
-              debug(err)
-            } else {
-              debug(res)
-            }
-          })
-        } else {
-          balance = bal
-        }
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ balance }))
-        res.on('finish', () => { clearTimeout(timer) })
-        debug('%s: Balance request completed successfully.', pkg.name)
-      })
-    } catch (err) {
-      debug(err)
-      res
-        .status(status.internalServerError)
-        .send('Balance request failed. \n')
-        .end()
-      debug(err)
-      clearTimeout(timer)
-    }
-  }
-
-  async function ontransfer(req, res) {
-    const now = new Date()
-
-    try {
-      if (0 !== req.params.did.indexOf('did:ara:')) {
-        req.params.did = `did:ara:${req.query.did}`
-      }
-      // Check Balance before processing Transfer request
-
-      info('%s: Transfer request for', pkg.name, req.params.did)
-      const recipient = req.params.did
-      const tokens = req.body.tokens || DEFAULT_TOKEN_COUNT
-      const balance = await token.balanceOf(req.params.did)
-      if ((parseInt(balance, 10) + parseInt(tokens, 10)) > MAX_TOKEN_PER_ACCOUNT) {
-        res.status(status.ok)
-        res.end(`Cannot transfer tokens. Only ${MAX_TOKEN_PER_ACCOUNT} allowed per user`)
-      } else {
-        try {
-          token.transfer({
-            did: process.env.DID,
-            password: process.env.pwd,
-            to: recipient,
-            val: tokens
-          }).then((data) => {
-            info(JSON.stringify(data))
-          })
-        } catch (err) {
-          debug(err)
-        }
-
-        res.status(status.ok)
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({
-          created_at: now,
-          did: recipient,
-          tokens_requested: tokens
-        }))
-        info('%s: Transfer request submitted successfully.', pkg.name)
-      }
-    } catch (err) {
-      warn(err)
-      res
-        .status(status.internalServerError)
-        .send('Transfer request failed. \n')
-        .end()
-      debug(err)
-    }
-  }
-
-  async function onredeem(req, res) {
-    const timer = setTimeout(() => {
-      res
-        .status(status.requestTimeout)
-        .send(msg.requestTimeout)
-    }, REQUEST_TIMEOUT)
-
-    try {
-      if (undefined === req.headers.authentication || conf.authenticationKey !== req.headers.authentication) {
-        res
-          .status(status.authenticationError)
-          .send(msg.authenticationFailed)
-          .end()
-        clearTimeout(timer)
-      } else {
-        if (0 !== req.params.did.indexOf('did:ara:')) {
-          req.params.did = `did:ara:${req.query.did}`
-        }
-        res.status(status.ok)
-        info('%s: Transfer request for', req.params.did)
-        const recipient = 'did:ara:89b83d3deab9507889710bb5dbaf5e863435c05193dcb128f6488e0bd42a492b'
-        const { tokens } = req.body
-        const password = req.body.passphrase
-
-        const receipt = token.transfer({
-          did: req.params.did,
-          password,
-          to: recipient,
-          val: tokens
-        })
-
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(receipt))
-        res.on('finish', () => { clearTimeout(timer) })
-        info('%s: Redeem request submitted successfully!!!!', pkg.name)
-      }
-    } catch (err) {
-      warn(err)
-      res
-        .status(status.internalServerError)
-        .send('Transfer request failed. \n')
-        .end()
-      debug(err)
-      clearTimeout(timer)
-    }
-  }
-
-  function createResponse(opts) {
-    return {
-      didDocument: opts.ddo,
-      didReference: parseDID(opts.did),
-      methodMetadata: {},
-      resolverMetadata: {
-        retrieved: new Date(),
-        duration: opts.duration,
-        driverId: 'did:ara',
-        driver: 'HttpDriver',
-      }
-    }
   }
 
   function onlisten() {
