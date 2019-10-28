e7f0210123b38b108532072400eaf050ab309b04
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2019-10-28 18:04:30 -0400

    feat(config): Add LS private API & ENV credentials

--- config/index.js
+++ config/index.js
@@ -1,3 +1,38 @@
+
+/*
+Set ENV variable
+ */
+
+let env = process.env.ENV
+
+if (process.env.ENV) {
+ env = process.env.ENV
+} else {
+   try {
+     env = fs.readFileSync(path.resolve(__dirname, 'env')).toString().trim() || env
+   } catch (e) {
+     env = 'production'
+   }
+}
+
+/*
+Set Littlstar Private API credentials
+ */
+
+const privateAPI = {}
+
+privateAPI.basePath = 'production' === env ?
+  'https://littlstar.com/api/private' :
+  'https://staging.littlstar.com/api/private'
+
+privateAPI.apiKey = 'production' === env ?
+  'ea60d365bef27b80a2f2fc019c3c1eaa0a38682d8e361ce935ad9a0f':
+  'f48dcf7e21b23d1146f48b409ee470b4120661a5fb9c373dc1174fd6'
+
+privateAPI.appToken = 'production' === env ?
+  '35d7df6f-fdd7-410f-82ed-5355d3729a1d':
+  '0b0e786c-ce0e-4367-9863-570284a4aae4'
+
 /*
 Status Variables
  */
@@ -27,9 +62,9 @@ Constant Values
  */
 
 const serverValues = {
-  MAX_TOKEN_PER_ACCOUNT: 500,
+  MAX_TOKEN_PER_ACCOUNT: 100,
   DEFAULT_TOKEN_COUNT: 100,
-  TRANSFER_TIMEOUT: 90000,
+  TRANSFER_TIMEOUT: 120000,
   REQUEST_TIMEOUT: 5000
 }
 
@@ -41,6 +76,7 @@ const gasAPI = "https://ethgasstation.info/json/ethgasAPI.json"
 
 module.exports = {
   serverValues,
+  privateAPI,
   gasAPI,
   status,
   msg
