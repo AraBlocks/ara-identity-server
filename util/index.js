89382f0661e45b37dff3c03323683b85bf47a29d
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2019-10-28 17:50:12 -0400

    feat(util): Add method to submit Transaction

--- util/index.js
+++ util/index.js
@@ -1,8 +1,10 @@
+const { submitTransaction } = require('./submitTransaction')
 const { getClientAuthKey } = require('./getClientAuthKey')
 const { getServerAuthKey } = require('./getServerAuthKey')
 const { getGasPrice } = require('./getGasPrice')
 
 module.exports = {
+  submitTransaction,
   getClientAuthKey,
   getServerAuthKey,
   getGasPrice
