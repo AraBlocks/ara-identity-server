00b5904cd715de2cf37424c80e5b76f1a1062950
Prashanth Balasubramani <prashanthbalasubramani@gmail.com>
2019-11-25 21:54:35 -0500

    feat(transfer): Add job queue login for submitting txns

--- util/submitTransaction.js
+++ util/submitTransaction.js
@@ -6,7 +6,7 @@ const { token } = require('ara-contracts')
 const { web3 } = require('ara-util')
 const request = require('superagent')
 const debug = require('debug')('ara:network:node:identity-manager:getGasPrice')
-const kue = require('kue')
+const queue = require('bull')
 
 const {
   basePath,
@@ -23,116 +23,87 @@ const {
   TRANSFER_TIMEOUT
 } = serverValues
 
-let jobQueue = kue.createQueue()
+let transferQueue = new queue('Ara Transfer', 'redis://127.0.0.1:6379')
 
-jobQueue.on('job enqueue', function(did, tokens){
-  console.log(`Transfer Job submitted for ${did} to transfer ${tokens} ARA`)
-})
+transferQueue.process((job) => {
 
-jobQueue.process(`transfer`, function(job, done){
+  return new Promise(async (resolve, reject) => {
+    try {
+      let { to, val } = job.data
 
-})
+      let { average, fast } = await getGasPrice()
+      info("Current Average Gas Price : ", average)
 
-/**
- * Submit a transaction to the blockchain
- * @param  {Object}   opts
- * @param  {String}   opts.to
- * @param  {String}   opts.val
- * @param  {String}   opts.gasPrice
- */
+      let txHash = null
+      let timer = null
 
-async function submitTransaction(opts) {
-  if (!opts.to || 'string' !== typeof opts.to) {
-    throw new TypeError(`Expected 'opts.to' to be non-empty Ara DID or Ethereum address string. Got ${opts.to}. Ensure ${opts.to} is a valid Ara identity.`)
-  } else if (!opts.val || 0 >= Number(opts.val)) {
-    throw new TypeError(`Expected 'opts.val' to be greater than 0. Got ${opts.val}. Ensure ${opts.val} is a positive number.`)
-  } else if (opts.gasPrice && ('number' !== typeof opts.gasPrice || opts.gasPrice < 0)) {
-    throw new TypeError(`Expected 'opts.gasPrice' to be a positive number. Got ${opts.gasPrice}.`)
-  }
-
-  // Get Current Gas Price
-  let { average, fast } = await getGasPrice()
-  debug("Current Average Gas Price : ", average)
-
-  let { to, val, gasPrice } = opts
-
-  if(!gasPrice) {
-    gasPrice = average/10
-  }
-
-  let txHash = null
-  let timer = null
-
-  function onhash(hash) {
-    txHash = hash
-    info("Transaction Hash : ", txHash)
-    timer = setTimeout(ontimeout, TRANSFER_TIMEOUT)
-  }
-
-  async function onmined() {
-    clearTimeout(timer)
-    const receipt = await web3.tx.getTransactionReceipt(txHash)
-
-    debug(receipt)
-    info('Transfer Request completed successfully for ', txHash)
-
-    // Get Updated Balance from the Blockchain
-    let balance = await token.balanceOf(to)
-    await redisClient.set(to, balance, 'EX', 60, (err, val) => {
-      if (err) {
-        debug(err)
-      } else {
-        debug(`Balance updated for ${to}`)
+      function onhash(hash) {
+        txHash = hash
+        info("Transaction Hash : ", txHash)
+        timer = setTimeout(ontimeout, TRANSFER_TIMEOUT)
       }
-    })
 
-    // Update Balance to Rails Backend
-    await request
-      .post(`${basePath}/ara_identities/callback`)
-      .send({ did: to, balance })
-      .set('X-Apikey', apiKey)
-      .set('X-AppToken', appToken)
-      .set('Accept', 'application/json')
-      .then((res) => {
-        if (200 === res.status) {
-          info(`Balance Updated in Rails Backend for ${to}`)
-        }
-      })
-      .catch((err) => {
-        debug(err)
-      })
-  }
+      async function onmined() {
+        clearTimeout(timer)
+        const receipt = await web3.tx.getTransactionReceipt(txHash)
 
-  function ontimeout() {
-    info('Transfer Request timed out....Submitting on a higher Gas Price')
-    token.transfer({
-      did,
-      password,
-      to,
-      val,
-      gasPrice: fast/10,
-      onhash,
-      onmined
-    })
-  }
-  // Submit Transfer Request to Blockchain
-  token.transfer({
-    did,
-    password,
-    to,
-    val,
-    gasPrice,
-    onhash,
-    onmined
+        debug(receipt)
+        info('Transfer Request completed successfully for ', txHash)
+
+        // Get Updated Balance from the Blockchain
+        let balance = await token.balanceOf(to)
+        await redisClient.set(to, balance, 'EX', 60, (err, val) => {
+          if (err) {
+            debug(err)
+          } else {
+            debug(`Balance updated for ${to}`)
+          }
+        })
+
+        // Update Balance to Rails Backend
+        await request
+          .post(`${basePath}/ara_identities/callback`)
+          .send({ did: to, balance })
+          .set('X-Apikey', apiKey)
+          .set('X-AppToken', appToken)
+          .set('Accept', 'application/json')
+          .then((res) => {
+            if (200 === res.status) {
+              info(`Balance Updated in Rails Backend for ${to}`)
+            }
+          })
+          .catch((err) => {
+            debug(err)
+          })
+
+        resolve()
+      }
+
+      function ontimeout() {
+        info('Transfer Request timed out....Submitting on a higher Gas Price')
+        token.transfer({
+          did,
+          password,
+          to,
+          val,
+          gasPrice: fast/10,
+          onhash,
+          onmined
+        })
+      }
+      // Submit Transfer Request to Blockchain
+      token.transfer({
+        did,
+        password,
+        to,
+        val,
+        gasPrice: average/10,
+        onhash,
+        onmined
+      })
+    } catch (err) {
+      console.log(err)
+      reject(err)
+    }
   })
-
-
-
-  return true
-}
-
-
-
-module.exports = {
-  submitTransaction
-}
+})
